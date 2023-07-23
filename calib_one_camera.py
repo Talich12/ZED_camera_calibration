eb342c3f18a9263e7c4fbbf640a2ed74a6b23c26
talich12 <denis.tal@mail.ru>
2023-07-24 01:12:46 +1000

    style(workspace): Исправление в соответствии с flake8

--- calib_one_camera.py
+++ calib_one_camera.py
@@ -13,36 +13,40 @@ class OneCameraCalibration:
         self._t = []
 
     def get_calibration(self, folder, string):
-        CHECKERBOARD = (6, 10)
+        CHECKERBOARD = (10, 6)
         criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
-        objpoints = [] # 3d point in real world space
-        imgpoints = [] # 2d points in image plane.
+        objpoints = []  # 3d point in real world space
+        imgpoints = []  # 2d points in image plane.
 
         objp = np.zeros((CHECKERBOARD[0]*CHECKERBOARD[1], 3), np.float32)
         objp[:, :2] = np.mgrid[0:CHECKERBOARD[0], 0:CHECKERBOARD[1]].T.reshape(-1, 2)
         objp *= 4
         for filename in os.listdir(folder):
-            if filename.endswith(".jpeg"): # you may specify other image formats
+            if filename.endswith(".jpeg"):  # you may specify other image formats
                 img_path = os.path.join(folder, filename)
                 img = cv2.imread(img_path)
                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
                 ret, corners = cv2.findChessboardCorners(gray, CHECKERBOARD, None)
 
                 # If found, add object points, image points (after refining them)
-                if ret == True:
+                if ret:
                     objpoints.append(objp)
-                    corners2 = cv2.cornerSubPix(gray,corners, (11,11), (-1,-1), criteria)
+                    corners2 = cv2.cornerSubPix(gray, corners, (11, 11), (-1, -1), criteria)
                     imgpoints.append(corners)
 
                     # Draw and display the corners
-                    cv2.drawChessboardCorners(img, (7,6), corners2, ret)
-                    # cv2.imshow('img', img)
-                    # cv2.waitKey(500)
+                    cv2.drawChessboardCorners(img, (7, 6), corners2, ret)
+                    cv2.imshow('img', img)
+                    cv2.waitKey(500)
 
         cv2.destroyAllWindows()
 
-        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None, None)
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints,
+                                                           imgpoints,
+                                                           gray.shape[::-1],
+                                                           None,
+                                                           None)
         print(mtx)
         print()
         print(dist)
@@ -65,9 +69,8 @@ class OneCameraCalibration:
 def main():
     calibration = OneCameraCalibration()
 
-    k1 = calibration.get_calibration("left_camera", "LEFT_CAM_FHD")
+    k1 = calibration.get_calibration("left", "LEFT_CAM_FHD")
     print(k1)
-
 
 
 if __name__ == "__main__":
